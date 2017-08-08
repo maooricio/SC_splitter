bfcaabc860a706d7f95e10d9a72454dd0469088e
Mauricio Cano Giraldo <lcanogiraldo@gmail.com>
2017-08-07 20:32:32 -0500

    Update 07-08-2017

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -3,63 +3,81 @@
 pragma solidity ^0.4.6;
 
 contract Splitter {
-  address      public owner;
-  address[2]   public participantsAdresses;
-  bool         public participantsEstablished;
-  bool         public contractDead;
-
-  event LogParticipantsAdresses(address[2] newParticipantsAddresses);
-  event LogDataOfTransaction(address addressOfSender, uint amountSent);
-  event LogAccountsBalances(address participantAddress1, uint balanceParticipant1, address participantAddress2, uint balanceParticipant2);
-  
-  function Splitter() {
-    owner = msg.sender;
-  }
-  
-  function contribute()
-      public payable
-      returns(bool successTransaction)
-  {
-    if(contractDead) throw;
-    if(msg.value == 0) throw;
-    if(msg.sender == owner) {
-      if(!participantsEstablished) throw;
-      if(!participantsAdresses[0].send(msg.value/2)) throw;
-      if(!participantsAdresses[1].send(msg.value/2)) throw;
-      LogAccountsBalances(participantsAdresses[0], participantsAdresses[0].balance, participantsAdresses[1], participantsAdresses[1].balance);
+    address     public owner;
+    address     public participantAddress1;
+    address     public participantAddress2;
+    bool        public contractDead;
+    
+    struct ParticipantStruct {
+        uint    authorizedAmountToWithdrawal;
+        bool    isParticipant;
     }
     
-    LogDataOfTransaction(msg.sender, msg.value);
-    return true;
-  }
-  
-  function seeParticipantsAddresses() constant returns (address[2] actualParticipantAddresses) {
-    return participantsAdresses;
-  }
-  
-  function addTwoParticipantsAccounts(address participantAddress1, address participantAddress2) 
-    public
-    returns(bool participantsAdded) 
-  {
-    if(contractDead) throw;
-    if(msg.sender != owner) throw;
-    if(participantAddress1 == 0 || participantAddress2 == 0) throw;
+    mapping(address => ParticipantStruct) public participantsStruct;
     
-    participantsAdresses[0] = participantAddress1;
-    participantsAdresses[1] = participantAddress2;
+    event LogDataOfTransaction(address addressOfSender, uint amountSent);
+    event LogDataOfWithdrawal(address addressOfParticipant, uint amountWithdrawn);
     
-    participantsEstablished = true;
-    LogParticipantsAdresses(participantsAdresses);
-    return true;
-  }
-  
-  function setContractState(bool stateOfContract) public returns(bool isContractAlive) {
-    if(msg.sender != owner) throw;
-    contractDead = stateOfContract;
-    return !contractDead;
-  }
-  
-  function seeContractState() constant returns (bool actualContractState) {
-    return contractDead;
-  }
+    function Splitter(address _participantAddress1, address _participantAddress2)
+    {
+        owner = msg.sender;
+        participantAddress1 = _participantAddress1;
+        participantAddress2 = _participantAddress2;
+        
+        participantsStruct[participantAddress1].authorizedAmountToWithdrawal = 0;
+        participantsStruct[participantAddress1].isParticipant = true;
+        
+        participantsStruct[participantAddress2].authorizedAmountToWithdrawal = 0;
+        participantsStruct[participantAddress2].isParticipant = true;
+    }
+    
+    function isParticipant(address participantAddress) 
+        public constant 
+        returns(bool authorizedParticipant) 
+    {
+        return participantsStruct[participantAddress].isParticipant;
+    }
+    
+    function depositFunds() 
+        public payable
+        returns(bool successTransaction) 
+    {
+        if(contractDead) throw;
+        if(msg.value == 0) throw;
+        if(msg.sender == owner) {
+            uint valueToWithdrawal = msg.value / 2;
+            if (msg.value % 2 == 1) valueToWithdrawal++;
+            
+            participantsStruct[participantAddress1].authorizedAmountToWithdrawal += valueToWithdrawal;
+            participantsStruct[participantAddress2].authorizedAmountToWithdrawal += valueToWithdrawal;
+        }
+        
+        LogDataOfTransaction(msg.sender, msg.value);
+        return true;
+    }
+    
+    function withdrawFunds() 
+        public
+        returns(bool successWithdrawal) 
+    {
+        if(contractDead) throw;
+        if(!isParticipant(msg.sender)) throw;
+        if(participantsStruct[msg.sender].authorizedAmountToWithdrawal < 0) throw;
+
+        msg.sender.transfer(participantsStruct[msg.sender].authorizedAmountToWithdrawal);
+        participantsStruct[msg.sender].authorizedAmountToWithdrawal = 0;
+        
+        LogDataOfWithdrawal(msg.sender, participantsStruct[msg.sender].authorizedAmountToWithdrawal);
+        return true;
+    }
+    
+    function setContractState(bool stateOfContract)
+        public 
+        returns(bool isContractAlive)
+    {
+        if(msg.sender != owner) throw;
+        contractDead = stateOfContract;
+        return !contractDead;
+    }
+        
 }
